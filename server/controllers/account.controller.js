a03fbf41067ad7735acaa5a1d8d714b187e2c50e
Malik <permalik@protonmail.com>
2022-06-29 20:17:33 -0500

    Build Account controller

---
+++ server/controllers/account.controller.js
@@ -0,0 +1,31 @@
+const db = require('../models');
+const Account = db.accounts;
+const Op = db.Sequelize.Op;
+
+exports.create = (req, res) => {
+
+};
+
+exports.findAll = (req, res) => {
+
+};
+
+exports.findOne = (req, res) => {
+
+};
+
+exports.update = (req, res) => {
+
+};
+
+exports.deleteAll = (req, res) => {
+
+};
+
+exports.delete = (req, res) => {
+
+};
+
+exports.findAllPublished = (req, res) => {
+
+};
